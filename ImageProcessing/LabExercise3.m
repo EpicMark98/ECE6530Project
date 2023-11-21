9091fcd13e0d33d540838a499daabf20fd226cdf
darren-rou <91917562+darren-rou@users.noreply.github.com>
2023-11-21 01:26:22 -0700

    go through most of lab

--- ImageProcessing/LabExercise3.m
+++ ImageProcessing/LabExercise3.m
@@ -33,3 +33,67 @@ plot(-pi:(2*pi/212):pi,abs(rowDownFreq));
 title('Frequency Response of Downsampled Image');
 xlabel('Frequency (rad/sample)');
 ylabel('Magnitude');
+
+%% 3.2
+xxSize = size(xx);
+xx3 = xx(1:3:xxSize(1), 1:3:xxSize(2));
+figure;
+% show_img(xx3);
+imshow(xx3);
+
+%% (a)
+
+xr1 = (-2) .^ (0:6);
+L = length(xr1);
+nn = ceil((0.999:1:4*L)/4);
+% Note nn holds the values for 4 samples: 1, 1, 1, 1, 2, 2, 2, 2, ...
+% The interpolation factor is 4
+xr1hold = xr1(nn);
+figure;
+stem(0:length(xr1)-1, xr1)
+title("Plot of xr1")
+figure;
+stem(0:length(xr1hold)-1, xr1hold)
+title("Plot of xr1 with 4-point zero order hold")
+
+%% (b) Zero-order hold
+
+% expand it horizontally, along the rows
+
+% when using size(), the return value is an array of 2 elements. The first
+% is the number of rows. The second is the number of elements.
+
+xx3Size = size(xx3);
+rowwiseSamplePoints = ceil((0.999:1:3*xx3Size(2))/3);
+xholdrows = zeros(xx3Size(1), 3*xx3Size(2));
+for i=1:xx3Size(1)
+    xholdrows(i, :) = xx3(i, rowwiseSamplePoints);
+end
+figure
+imshow(xholdrows)
+
+%% (c) 
+xholdrowsSize = size(xholdrows);
+columnwiseSamplePoints = ceil((0.999:1:3*xholdrowsSize(1))/3);
+xhold = zeros(3*xholdrowsSize(1), xholdrowsSize(2));
+for j=1:xholdrowsSize(2)
+    xhold(:, j) = xholdrows(columnwiseSamplePoints, j);
+end
+figure
+imshow(xhold)
+
+%% (d)
+n1 = 0:6;
+xr1 = (-2).^n1;
+tti = 0:0.1:6;   %-- locations between the n1 indices
+xr1linear = interp1(n1,xr1,tti);   %-- function is INTERP-ONE
+figure
+stem(tti,xr1linear)
+% As can be seen by tti = 0:0.1:6;, the interpolation factor is 1/0.1 = 10.
+
+xxlinear = linearInterpolation(xx3, 3);
+figure
+imshow(xxlinear)
+title("Image after downsampling and linear interpolation")
+
+
---
+++ ImageProcessing/linearInterpolation.m
@@ -0,0 +1,21 @@
+function y = linearInterpolation(x, interpolationFactor)
+    % This function takes a matrix x and an interpolation factor
+    % interpolationFactor.
+    xSize = size(x);
+    % if the original size is N, and the interpolation factor is a, the 
+    % result after interpolation is of size (N-1)*a + 1.
+    y1 = zeros(xSize(1), interpolationFactor*(xSize(2)-1) + 1);
+    rowwiseSamplePoints = 1:1/interpolationFactor:xSize(2);
+    for i=1:xSize(1)
+        y1(i, :) = interp1(1:xSize(2), x(i, :), rowwiseSamplePoints);
+    end
+    y1 = y1'; % trasnpose 
+    
+    y1Size = size(y1);
+    y2 = zeros(y1Size(1), interpolationFactor*(y1Size(2)-1) + 1);
+    columnwiseSamplePoints = 1:1/interpolationFactor:y1Size(2);
+    for j=1:y1Size(1)
+        y2(j, :) = interp1(1:y1Size(2), y1(j, :), columnwiseSamplePoints);
+    end
+    y = y2';
+end
