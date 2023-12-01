40b96dd9bcd67310c5549de5191b1dac528e72c4
NoahLomuASU <nlomu@asu.edu>
2023-12-01 12:37:10 -0700

    small changes to S-8 2.3

--- ChirpSpectrogram/LabS-8_Exercise2.3/main.m
+++ ChirpSpectrogram/LabS-8_Exercise2.3/main.m
@@ -5,7 +5,7 @@ load('48000_bicycle_built_for_two.mat', 'data');
 fSamp = 48000;
 vv = data(370000:end, 1)'; % gets the left channel, second half of audio
 tSect = 0.025;
-lSect = round(tSect*fSamp);
+lSect = round(tSect*fSamp); % must be an integer
 soundsc(vv, fSamp);
 figure
 plotspecDB(vv + 1j*1e-12, fSamp, lSect, 80)
@@ -18,3 +18,13 @@ title("I'm half crazy all for the love of you")
 %
 % <<voice_spectrogram.png>>
 %
+
+%% 2.3.2)
+% The short 'a' vowel has a fundamental frequency of about 200Hz. This
+% corresponds to a fundamental period of 5ms.
+
+%% 2.3.3)
+% N/A
+
+%% 2.3.4)
+% 
